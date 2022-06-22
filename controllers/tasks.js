88d3bfdc71000e1cdc8ae99f42c2492ab982f864
Aritra Biswas <aritrabiswas123.kulti@gmail.com>
2022-06-22 20:48:57 +0530

    completing crud functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -27,11 +27,37 @@ const postTask = async (req, res) => {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
 }
-const updateTask = (req, res) => {
-    res.send(`update task no. ${req.params.id}`);
+const deleteTask = async (req, res) => {
+    // res.send("delete Task");
+    try {
+        const taskID = req.params.id;
+        const task = await Task.deleteOne({_id : taskID}) ;
+        if(!task){
+            return res.status(404).json({msg: `no task with id: ${taskID}`});
+        }
+    } catch (error) {
+        res.status(500).json({msg : error});
+    }
+    
 }
-const deleteTask = (req, res) => {
-    res.send("delete Task");
+const updateTask = async (req, res) => {
+    // res.send(`update task no. ${req.params.id}`);
+    try {
+        const {id : taskID} = req.params;
+        const updatedTask = await Task.findOneAndUpdate({_id: taskID}, req.body,{
+            new: true, // this option is required if we want a new updated object.
+            runValidators: true,
+        });
+        if(!updatedTask){
+            return res.status(404).json({msg: `no task with id: ${taskID}`});
+        }
+        res.status(200).json({ updatedTask });
+        
+    } catch (error) {
+        res.status(500).json({msg : error});
+    }
+     
+
 }
 
 module.exports = {
